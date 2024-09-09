975dbcb14ae21801fa351ab8edf18677a2b78503
Andrea-Asaro <andrea.asaro.24@gmail.com>
2024-09-09 19:45:28 +0200

    modificato sezione chisiamo per suite, da vedere se va bene o aggiungere png di sfondo

--- src/components/ChiSiamo.jsx
+++ src/components/ChiSiamo.jsx
@@ -17,14 +17,14 @@ export default function ChiSiamo() {
     });
 
     return (
-        <div className="container-fluid my-5">
-            <div className="row imgBand" ref={ref}>
-                <div className="col-12 shadeBand">
+        <div className="container-fluid">
+            <div className="row whiteBand" ref={ref}>
+                <div className="col-12 py-5 imageBand">
                     {/* Applica l'animazione ai componenti */}
-                    <animated.h3 style={fadeUpStyle} className="z-2 h1 pt-5 text-center fw-bold text2 font1">
-                        CHI SIAMO
-                    </animated.h3>
-                    <animated.p style={fadeUpStyle} className="text0 z-2 mt-4 text-center fs-4 lorem font2">
+                    <animated.h2 style={fadeUpStyle} className="z-2 animatedh2 pt-5 text-center fw-bold text1 font1">
+                        Chi siamo
+                    </animated.h2>
+                    <animated.p style={fadeUpStyle} className="text3 z-2 mt-4 mb-5 text-center fs-4 lorem font2">
                         Scopri il fascino del Copacabana Colors, il B&B che porta la Puglia nella tua vacanza. A due passi dal mare, ti accoglie con camere luminose e curate nei dettagli, dove ogni comfort è pensato per farti sentire come a casa, con un tocco di stile e autenticità.
                     </animated.p>
                 </div>
